6f5df989514858383f55a836c356d3da9575241a
Nickkirui <nickkirui69@gmail.com>
2024-01-16 20:18:21 +0300

    Trivia App

--- src/App.js
+++ src/App.js
@@ -77,26 +77,48 @@ function App() {
         },
       ],
     },
+    {
+      id: 3,
+      question: "which country won the FIFA World Cup 2018?",
+      answers: [
+        {
+          text: "Argentina",
+          correct: false,
+        },
+        {
+          text: "Brazil",
+          correct: false,
+        },
+        {
+          text: "England",
+          correct: false,
+        },
+        {
+          text: "France",
+          correct: true,
+        },
+      ],
+    },
   ];
 
   const moneyPyramid = useMemo(
     () =>
       [
-        { id: 1, amount: "$ 100" },
-        { id: 2, amount: "$ 200" },
-        { id: 3, amount: "$ 300" },
-        { id: 4, amount: "$ 500" },
-        { id: 5, amount: "$ 1.000" },
-        { id: 6, amount: "$ 2.000" },
-        { id: 7, amount: "$ 4.000" },
-        { id: 8, amount: "$ 8.000" },
-        { id: 9, amount: "$ 16.000" },
-        { id: 10, amount: "$ 32.000" },
-        { id: 11, amount: "$ 64.000" },
-        { id: 12, amount: "$ 125.000" },
-        { id: 13, amount: "$ 250.000" },
-        { id: 14, amount: "$ 500.000" },
-        { id: 15, amount: "$ 1.000.000" },
+        { id: 1, amount: "Ksh 100" },
+        { id: 2, amount: "Ksh 200" },
+        { id: 3, amount: "Ksh 300" },
+        { id: 4, amount: "ksh 500" },
+        { id: 5, amount: "Ksh 1.000" },
+        { id: 6, amount: "Ksh 2.000" },
+        { id: 7, amount: "ksh 4.000" },
+        { id: 8, amount: "Ksh 8.000" },
+        { id: 9, amount: "Ksh 16.000" },
+        { id: 10, amount: "Ksh 32.000" },
+        { id: 11, amount: "Ksh 64.000" },
+        { id: 12, amount: "Ksh 125.000" },
+        { id: 13, amount: "ksh 250.000" },
+        { id: 14, amount: "ksh 500.000" },
+        { id: 15, amount: "ksh 1.000.000" },
       ].reverse(),
     []
   );
